f684c5ace57b1906de7b1f165e2d24ea09774f2a
Viraj Kadam <hivirajkadam@gmail.com>
2023-11-08 23:19:59 +0530

    'Viraj: pull'

--- command.sh
+++ command.sh
@@ -1,12 +1,12 @@
-git init
-git config core.fileMode false
-git config core.autocrlf false
-git add --a
-git commit -m "'$1'"
-git rm -r --cached .
-git add --a
-git commit -m "'$1'"
-git pull origin main
-chmod -R 777 ../ultrasuedegreen.com/
-chown -R www-data:www-data ../ultrasuedegreen.com/
-git push origin HEAD:main
+git init;
+git config core.fileMode false;
+git config core.autocrlf false;
+git add --a;
+git commit -m "'$1'";
+git rm -r --cached .;
+git add --a;
+git commit -m "'$1'";
+git pull origin main;
+chmod -R 777 ../ultrasuedegreen.com/;
+chown -R www-data:www-data ../ultrasuedegreen.com/;
+git push origin HEAD:main;
