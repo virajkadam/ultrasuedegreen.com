33a85dae22e113fed3d1c7f92058d20d39254138
Viraj Kadam <hivirajkadam@gmail.com>
2023-11-08 23:18:46 +0530

    Viraj: pull push

--- command.sh
+++ command.sh
@@ -1,18 +1,12 @@
 git init
-
 git config core.fileMode false
 git config core.autocrlf false
-
-git add .
+git add --a
 git commit -m "'$1'"
-
 git rm -r --cached .
-git add .
+git add --a
 git commit -m "'$1'"
-
 git pull origin main
-
 chmod -R 777 ../ultrasuedegreen.com/
 chown -R www-data:www-data ../ultrasuedegreen.com/
-
 git push origin HEAD:main
